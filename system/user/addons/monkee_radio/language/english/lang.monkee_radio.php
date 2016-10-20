55881abc6bb1ad855c13ef559eff4f6d2ffe474e
Monk-ee <john@omahamediagroup.com>
2016-10-20 15:14:27 -0500

    Create lang.monkee_radio.php

---
+++ system/user/addons/monkee_radio/language/english/lang.monkee_radio.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Monk-ee Radio Buttons - Language
+ */
+
+$lang = array(
+//Field Info
+'monkrad_default_field_name' => "Monk-ee Radio Buttons",
+'monkrad_default_field_desc' => "Modified version of the default Radio Buttons fieldtype.",
+'monkrad_default_field_type' => "monkee_radio",
+
+//Settings
+'monkrad_default_value' => "Default Value",
+'monkrad_default_value_desc' => "If form data is not being edited or there is no saved data, this option will be selected by default. For a Value/Label list, enter the value. NOTE: Setting the inline 'default_value' parameter will override these settings.",
+'monkrad_enable_autofocus' => "Enable Autofocus",
+'monkrad_enable_autofocus_desc' => "Field will automatically be selected on page load. If multiple fields on a form have this property enabled, the field loaded first will claim control. For radio buttons, the focus will be on the first option.",
+'monkrad_enable' => "Enable",
+'monkrad_field_title' => "Populate Title Parameter",
+'monkrad_field_title_desc' => "Select if and how you want to populate the title field in the input tag. This is useful for displaying tooltips. This will be applied to the unordered list containing the radio buttons. NOTE: Setting the inline 'attr:title' parameter will override these settings.",
+'monkrad_field_none' => "None",
+'monkrad_field_label' => "Field Label",
+'monkrad_field_desc' => "Field Description",
+'monkrad_field_custom' => "Custom",
+'monkrad_field_title_custom' => "Enter custom title here",
+'monkrad_css_classes' => "CSS Classes",
+'monkrad_css_classes_desc' => "Output as many classes as you like. The checkboxes will add dynamic information as classes. Enter any custom classes in the text box. This will be applied to the unordered list containing the radio buttons. NOTE: Setting the inline 'attr:class' parameter will override these settings. Field Name pulls from the Field Name value shown above. Freeform Field Type will output 'monkee_radio'",
+'monkrad_css_name' => "Field Name",
+'monkrad_css_type' => "Freeform Field Type",
+'monkrad_css_custom' => "Enter custom css styles here",
+'monkrad_custom_params' => "Custom Parameters",
+'monkrad_custom_params_desc' => "Enter custom parameters and values here. Values not required. These will be added to the field tag.",
+'monkrad_custom_place' => "Parameter",
+'monkrad_custom_value' => "Value",
+'monkrad_js_event' => "Custom Javascript/jQuery Event",
+'monkrad_js_event_desc' => "For advanced users only. Select a javascript event from the dropdown box. Then, in the textbox, enter the javascript (or jQuery) you would like to attach to this event. This will be applied to the unordered list containing the radio buttons. <b>PLEASE NOTE: Because of the way Freeform handles field entry on this page, you must escape parentheses with a backslash. Failing to do this will result in this form not saving. Also, DO NOT USE DOUBLE QUOTES, only single quotes. Ex: alert&#92;('test'&#92;);.</b> <a href='http://www.w3schools.com/jsref/dom_obj_event.asp' target='_blank'>Click here to view a list of javascript events and descriptions.</a> NOTE: Setting the inline 'attr:onclick' parameter will override an onclick action setup here.",
+'monkrad_js_action' => "Enter JavaScript/jQuery action here",
+'monkrad_js_confirm' => "I confirm that I am an advanced user and have read the instructions.",
+
+//END
+'' => ''
+);
